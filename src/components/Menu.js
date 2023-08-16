9adc4b9208dc005d614d7c087cf8e6f24a2b593a
Francisco Bignotto <franciscogrynberg34@gmail.com>
2023-08-15 23:07:46 -0300

    v1.0.5

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,7 +10,7 @@ function Menu() {
         <div className="menu">
             <Link to="/"><img className="menu__logo slit-in-diagonal-1" src="/images/logo/logo (verde medio).png" alt="" /></Link>
             <div className="menu__list">
-                <Link className={path === "/"? "menu__list__item menu__active fade-in-tl": "menu__list__item fade-in-tl" } to="/">portfólio</Link>
+                <Link className={path === "/"? "menu__list__item menu__active fade-in-tl": "menu__list__item fade-in-tl" } to="/">gryn</Link>
                 <Link className={path === "/stack"? "menu__list__item menu__active fade-in-tl": "menu__list__item fade-in-tl" } to="/stack">stack</Link>
                 <Link className={path === "/sobre"? "menu__list__item menu__active fade-in-tl": "menu__list__item fade-in-tl" } to="/sobre">sobre</Link>
             </div>
